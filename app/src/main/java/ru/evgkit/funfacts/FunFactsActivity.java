f9ba0cf69a6e9b46fdb228f119c93b11b18610a1
evgkit <kitaev.ea@gmail.com>
2015-09-13 20:00:49 +0300

    Hardcode facts

--- app/src/main/java/ru/evgkit/funfacts/FunFactsActivity.java
+++ app/src/main/java/ru/evgkit/funfacts/FunFactsActivity.java
@@ -2,13 +2,10 @@ package ru.evgkit.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
 
@@ -24,36 +21,9 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // The button was clicked, so update the fact label with the new fact
-                Random randomGenerator = new Random();
-                Integer factIndex = randomGenerator.nextInt(3) + 1;
-                String fact = factIndex != 3 ? factIndex.toString() : "Ostriches can run faster then the horses.";
-
-                factLabel.setText(fact);
+                factLabel.setText(new FactBook().getFact());
            }
         };
         showFactBtn.setOnClickListener(listener);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
